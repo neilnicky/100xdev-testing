dbc2b25b317212f735e7bc8806b71090e9f164e2
neilnicky <neilnicholaskanat@gmail.com>
2025-01-19 15:33:31 +0530

    feat: mock prisma client

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import { prismaClient } from "./db";
+import { z } from "zod";
 
 export const app = express();
 app.use(express.json());
+
+const sumInput = z.object({
+  a: z.number(),
+  b: z.number(),
+});
 
 app.post("/multiply", async (req, res) => {
   const a = req.body.a;
@@ -27,4 +33,30 @@ app.post("/multiply", async (req, res) => {
   });
 
   res.json({ answer: result });
+});
+
+app.post("/sum", async (req, res) => {
+  const parsedResponse = sumInput.safeParse(req.body);
+
+  if (!parsedResponse.success) {
+    res.status(411).json({
+      message: "Incorrect inputs",
+    });
+    return;
+  }
+
+  const answer = parsedResponse.data.a + parsedResponse.data.b;
+
+  await prismaClient.request.create({
+    data: {
+      a: parsedResponse.data.a,
+      b: parsedResponse.data.b,
+      answer: answer,
+      type: "Sum",
+    },
+  });
+
+  res.json({
+    answer,
+  });
 });
